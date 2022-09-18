3b779436e50911b630927a1f806289f518f4bcb8
Ashe Urban <ashe@goldentongue.com>
2022-09-18 14:59:58 -0700

    WIP import font.

--- src/components/InventoryDetail.js
+++ src/components/InventoryDetail.js
@@ -2,6 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function InventoryDetail(props){
+  const fancyBtn = {
+    backgroundColor:'lavender',
+    color:'black',
+    fontFamily: 'Jost',
+    fontSize: '20px',
+    padding: '10px 30px',
+    borderRadius: '5px',
+    margin: '10px 0px',
+    cursor:'pointer'
+  };
+
   const { inventory, inventoryWeight, onClickingMinusOne } = props;
 
   return (
@@ -9,7 +20,7 @@ function InventoryDetail(props){
       <h1>Inventory Detail</h1>
       <h3>Item: {inventory.item} | Base Weight: {inventory.weight} | Remaining Weight: {inventory.inventoryWeight} </h3>
       <p><em>{inventory.description}</em></p>
-      <button onClick={ props.onClickingMinusOne }>Sell 1 lb</button>
+      <button style={fancyBtn} onClick={ props.onClickingMinusOne }>Sell 1 lb</button>
       <hr/>
     </React.Fragment>
   );
